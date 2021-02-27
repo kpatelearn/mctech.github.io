69d15f4d5abd6c1e81b6af3b2a4dfd440e7486ac
kpatelearn <57775358+kpatelearn@users.noreply.github.com>
2021-02-27 16:05:08 +1300

    Update script.js
    Complete replacement

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-$(function(){
-  var str = '#len'; //increment by 1 up to 1-nelemnts
-  $(document).ready(function(){
-    var i, stop;
-    i = 1;
-    stop = 4; //num elements
-    setInterval(function(){
-      if (i > stop){
-        return;
-      }
-      $('#len'+(i++)).toggleClass('bounce');
-    }, 500)
-  });
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
 });
