11e8df3224c6d6688770115a4fb62bcc5bc0d204
kpatelearn <kpaterson1881@gmail.com>
2022-03-26 02:41:54 +1300

    css updates

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
-});
+      /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+      function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
